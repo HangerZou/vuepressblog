c2577fd8555f609dd0b62cf37aaf8bbf82e51ef5
HangerZou <929675848@qq.com>
2021-11-28 16:37:02 +0800

    搭建

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,10 @@ module.exports = {
     title: '昕翰博客',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
       ['link', { rel: 'icon', href: '/avatar.png' }], // 增加一个自定义的 favicon(网页标签的图标)
+        ['link', { rel: 'manifest', href: '/photo.jpg' }],
+        ['link', { rel: 'apple-touch-icon', href: '/photo.jpg' }],
     ],
+    serviceWorker: true,  // 是否开启 PWA
     plugins: ['demo-container'], // 配置插件
     themeConfig: {
       logo: '/avatar.png',  // 左上角logo
